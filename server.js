565e855a7b4d7b657c325fb96afebe0133522476
Christopher Lee <xchrislee@gmail.com>
2018-01-07 20:06:00 -0800

    Create function to test if fatsecret module working properly

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const fatSecret = require('./server/fatSecret');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/api/auth', fatSecret.foodSearch);
 
 app.listen(1234, () => {
   console.log('%s listening at %d', app.name, 1234);
