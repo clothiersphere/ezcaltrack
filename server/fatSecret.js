fa01d4acb0e66b7a50b34b7eeb018078bf1635c8
Christopher Lee <xchrislee@gmail.com>
2018-01-07 20:26:01 -0800

    Disable eslint - global require for fatsecret require/instatiation

--- server/fatSecret.js
+++ server/fatSecret.js
@@ -1,37 +1,18 @@
-const axios = require('axios');
 const keys = require('../keys.js');
-
-const KEY = keys.consumerKey;
-const SECRET = keys.sharedSecretKey;
-const fatAPI = new (require('fatsecret'))(KEY, SECRET);
-
-const url =  'http://platform.fatsecret.com/rest/server.api';
-// const encodeUrl = encodeURIComponent(url);
-
-// const a = foo;
-// const z= bar;
-// const consumerKey = keys.apiKey;
-// const signatureMethod = 'HMAC-SHA1';
-// const timestamp = Math.floor(Date.now() / 1000);
-// const nonce = 'abc';
-// const version = '1.0';
-
-// const params = `a=foo&oauth_consumer_key=${consumerKey}&oauth_nonce=${nonce}&oauth_signature_method=${signatureMethod}&oauth_timestamp=${timestamp}&oauth_version=${version}&z=bar`;
-// const encodeParams = encodeURIComponent(params);
-
-// const signatureBaseString = `POST&${encodeUrl}&${encodeParams}`;
-// const text = signatureBaseString;
-// const key = `${keys.consumerKey}&${keys.sharedSecretKey}`;
+/* eslint-disable global-require */
+const fatAPI = new (require('fatsecret'))(keys.consumerKey, keys.sharedSecretKey);
+/* eslint-disable global-require */
 
 const foodSearch = (req, res, next) => {
   fatAPI
     .method('foods.search', {
-      search_expression: 'Coffiest', 
-      max_results: 10
+      search_expression: 'Coffiest',
+      max_results: 10,
     })
-    .then(function(results) {
+    .then((results) => {
       console.log(results.foods.food);
-      res.send(results.foods.food);
+      const data = results.foods.food;
+      res.send(data);
       next();
     })
     .catch(err => console.error(err));
