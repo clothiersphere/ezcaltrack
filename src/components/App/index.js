b77c118f8bf32b0a054c589e3057a16c366f88ee
Christopher Lee <xchrislee@gmail.com>
2018-01-08 18:08:05 -0800

    Change App into smart component and implement Body component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Body from './Body';
 
-const App = () => {
-  return (
-    <div>
-      <p> APP </p>
-    </div>
-  )
+class App extends Component {
+  componentDidRender() {
+    console.log(this.props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Header
+        <Body />
+        Footer
+      </div>
+    );
+  }
 }
 
 export default App;
