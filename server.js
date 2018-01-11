3c33e620e2c68b7538361820b8a8a8c500e25143
Christopher Lee <xchrislee@gmail.com>
2018-01-10 18:18:48 -0800

    Fix typo

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const fatSecret = require('./server/fatSecret');
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/api/auth', fatSecret.foodSearch);
+app.get('/api/foodSearch', fatSecret.foodSearch);
 
 app.listen(1234, () => {
   console.log('%s listening at %d', app.name, 1234);
