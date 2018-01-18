f5aed54cf8cb03fbb4d73e0f0ef87771c6c66fec
Christopher Lee <xchrislee@gmail.com>
2018-01-17 16:56:04 -0800

    Implement onSearchKeyDown() and handleChange() functions

--- src/components/App/Header/Menu.js
+++ src/components/App/Header/Menu.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Dropdown, Icon, Menu, Segment, Button } from 'semantic-ui-react';
 
+class HeaderMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+  }
 
-// const foodSearch = () => {
+  onSearchKeyDown(e) {
+    if (e.key === 'Enter') {
+      this.props.foodSearch(this.state.term);
+    }
+  }
 
-// }
+  handleChange(e) {
+    this.setState({ term: e.target.value });
+  }
 
-
-const addEntryButton = () => (
+  render() {
+    const addEntryButton = (
       <div className="addEntryButton">
-        <Button animated="vertical" onClick={()=>console.log("add entry")}>
+        <Button animated="vertical" onClick={() => console.log("add entry")}>
           <Button.Content hidden><Icon name="plus" /></Button.Content>
           <Button.Content visible>
             Add Entry
@@ -17,45 +28,49 @@ const addEntryButton = () => (
         </Button>
       </div>
     );
-
-const HeaderMenu = ({foodSearch}) => {
-  console.log(foodSearch, "foodSearch")
-  return (
-  <div>
-    <Menu attached="top">
-      <Dropdown item icon="plus" simple>
-        <Dropdown.Menu>
-          <Dropdown.Item>
-            <Icon name="dropdown" />
-            <span className='text'>New</span>
+    return (
+      <div>
+        <Menu attached="top">
+          <Dropdown item icon="plus" simple>
             <Dropdown.Menu>
-              <Dropdown.Item>Document</Dropdown.Item>
-              <Dropdown.Item>Image</Dropdown.Item>
+              <Dropdown.Item>
+                <Icon name="dropdown" />
+                <span className="text">New</span>
+                <Dropdown.Menu>
+                  <Dropdown.Item>Document</Dropdown.Item>
+                  <Dropdown.Item>Image</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown.Item>
+              <Dropdown.Item>Open</Dropdown.Item>
+              <Dropdown.Item>Save...</Dropdown.Item>
+              <Dropdown.Item>Edit Permissions</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Header>Export</Dropdown.Header>
+              <Dropdown.Item>Share</Dropdown.Item>
             </Dropdown.Menu>
-          </Dropdown.Item>
-          <Dropdown.Item>Open</Dropdown.Item>
-          <Dropdown.Item>Save...</Dropdown.Item>
-          <Dropdown.Item>Edit Permissions</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Header>Export</Dropdown.Header>
-          <Dropdown.Item>Share</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-      <Menu.Item>
-        {addEntryButton()}
-      </Menu.Item>
-      <Menu.Menu position="right">
-        <div className="ui right aligned category search item">
-          <div className="ui transparent icon input">
-            <input className="prompt" type="text" placeholder="Search foods..." />
-            <i className="search link icon" />
-          </div>
-          <div className="results" />
-        </div>
-      </Menu.Menu>
-    </Menu>
-  </div>
-);
+          </Dropdown>
+          <Menu.Item>
+            { addEntryButton }
+          </Menu.Item>
+          <Menu.Menu position="right">
+            <div className="ui right aligned category search item">
+              <div className="ui transparent icon input">
+                <input
+                  className="prompt"
+                  type="text"
+                  onChange={e => this.handleChange(e)}
+                  onKeyDown={e => this.onSearchKeyDown(e)}
+                  placeholder="Search foods..."
+                />
+                <i className="search link icon" type="submit" value={this.state.term} />
+              </div>
+              <div className="results" />
+            </div>
+          </Menu.Menu>
+        </Menu>
+      </div>
+    );
+  }
 }
 
 export default HeaderMenu;
