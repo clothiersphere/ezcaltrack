4d258dd1ecf38423b93f7f01910f3272b1ab7278
Christopher Lee <xchrislee@gmail.com>
2018-01-18 15:11:47 -0800

    Implement prop validation for FoodSearchResults

--- src/components/App/Body/FoodSearchResults.js
+++ src/components/App/Body/FoodSearchResults.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const FoodSearchResults = ({ results }) => {
   let searchResults;
 
   if (Object.keys(results).length > 0) {
-    searchResults = Object.keys(results).map((key, i) => {
-      return (
+    searchResults = Object.keys(results).map((key, i) =>
+      (
         <div key={i} onClick={()=>console.log(results[key].food_id)}>
           {results[key].food_name} {results[key].food_description}
         </div>
-      );
-    });
+      ));
   } else {
     searchResults = <div> bye </div>;
   }
 
   return <div>{searchResults}</div>;
+};
+
+FoodSearchResults.propTypes = {
+  results: PropTypes.shape({
+    food_description: PropTypes.string,
+    food_id: PropTypes.number,
+    food_name: PropTypes.string,
+    food_type: PropTypes.string,
+    food_url: PropTypes.string,
+  }).isRequired,
 };
 
 export default FoodSearchResults;
