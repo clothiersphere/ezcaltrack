8773fb709af5cac2ba1f9679af165b7d677fc757
Christopher Lee <xchrislee@gmail.com>
2018-01-19 14:20:45 -0800

    Implement switch route for foodSearchResults component

--- src/components/App/Body/index.js
+++ src/components/App/Body/index.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react';
-import { Button, Icon } from 'semantic-ui-react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import FoodSearchResults from './FoodSearchResults';
 
 export default class Body extends Component {
   componentDidMount() {
     console.log(this, "body")
   }
-
-  iterate(props) {
-    const bleh = Object.keys(props).map((key, i) =>
-      (
-        <div key={i} onClick={()=>console.log(props[key].food_id)}>
-          {props[key].food_name} {props[key].food_description}
-        </div>
-      ),
-    );
-    return bleh;
-  }
-
   render() {
-    const { props } = this;
+    const { foodSearchResults } = this.props;
     return (
       <div>
-        {this.iterate(props)}
+        <Switch>
+          <Route exact path="/" render={routeProps => <FoodSearchResults {...routeProps} foodSearchResults={foodSearchResults} />} />
+        </Switch>
       </div>
     );
   }
