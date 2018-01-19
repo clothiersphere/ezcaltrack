f09c0c2ffcd9745e101f130e3cb60c16da4b35fe
Christopher Lee <xchrislee@gmail.com>
2018-01-19 14:23:58 -0800

    Implement logic to display dropdown w/ food search results

--- src/components/App/Body/FoodSearchResults.js
+++ src/components/App/Body/FoodSearchResults.js
@@ -1,31 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Button, Icon, Label, Dropdown } from 'semantic-ui-react';
 
-const FoodSearchResults = ({ results }) => {
-  let searchResults;
 
-  if (Object.keys(results).length > 0) {
-    searchResults = Object.keys(results).map((key, i) =>
-      (
-        <div key={i} onClick={()=>console.log(results[key].food_id)}>
-          {results[key].food_name} {results[key].food_description}
-        </div>
-      ));
-  } else {
-    searchResults = <div> bye </div>;
+export default class FoodSearchResults extends Component {
+  render() {
+    let searchResults;
+    const { foodSearchResults } = this.props;
+    if (foodSearchResults.length > 0) {
+      // if (Object.keys(results).length > 0) {
+      // searchResults = Object.keys(results).map((key, i) =>
+      //   (
+      //     <div key={i} onClick={()=>console.log(results[key].food_id)}>
+      //       {results[key].food_name} {results[key].food_description}
+      //     </div>
+      //   ));
+      const handleChange = (e, data) => {
+        console.log(data.value, "data value")
+      }
+
+      searchResults = <Dropdown 
+                        placeholder='Select Food' 
+                        fluid 
+                        selection 
+                        onChange={handleChange} 
+                        options={foodSearchResults} />
+    } else {
+      searchResults = <div> bye </div>;
+    }
+    return (
+      <div>{searchResults}</div>
+    );
   }
+}
 
-  return <div>{searchResults}</div>;
-};
+// FoodSearchResults.propTypes = {
+//   results: PropTypes.shape({
+//     food_description: PropTypes.string,
+//     food_id: PropTypes.number,
+//     food_name: PropTypes.string,
+//     food_type: PropTypes.string,
+//     food_url: PropTypes.string,
+//   }).isRequired,
+// };
 
-FoodSearchResults.propTypes = {
-  results: PropTypes.shape({
-    food_description: PropTypes.string,
-    food_id: PropTypes.number,
-    food_name: PropTypes.string,
-    food_type: PropTypes.string,
-    food_url: PropTypes.string,
-  }).isRequired,
-};
-
-export default FoodSearchResults;
+// export default FoodSearchResults;
