ac9389b8b032e0d940b52a51a2c54a0817965082
Christopher Lee <xchrislee@gmail.com>
2018-01-20 14:24:08 -0800

    Update function names for clarity

--- src/reducers/getFoodSearch.js
+++ src/reducers/getFoodSearch.js
@@ -2,16 +2,16 @@ import * as actionTypes from '../constants/actionTypes';
 
 const initialState = [];
 
-function getFoodSearch(state, action) {
+function updateFoodSearchState(state, action) {
   const food = action.results;
   return [...state, ...food];
 }
 
-export default function (state = initialState, action) {
+export default function getFoodSearch(state = initialState, action) {
   const newState = Object.assign({}, state);
   switch (action.type) {
     case actionTypes.GET_FOOD_SEARCH:
-      return getFoodSearch(newState, action);
+      return updateFoodSearchState(newState, action);
     default:
       return state;
   }
