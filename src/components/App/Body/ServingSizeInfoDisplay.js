02a374b666f6698dd0bf7ca135b719ee94ced4dc
Christopher Lee <xchrislee@gmail.com>
2018-01-28 12:19:40 -0800

    Start to implement and style Nutrition Facts Area.

--- src/components/App/Body/ServingSizeInfoDisplay.js
+++ src/components/App/Body/ServingSizeInfoDisplay.js
@@ -6,19 +6,41 @@ const ServingSizeInfoDisplay = (data) => {
   if (Object.keys(data).length > 0) {
     const selectedServing = serving => (
       <div>
-        {serving.name}
-        {serving.serving_description}
-        <div className="servingCalories">
-          {serving.calories} calories
+        {serving.metric_serving_amount} {serving.metric_serving_unit}
+        <div className="servingSizeBox">
+          <div className="servingSizeBoxTop">
+            <div className="servingCalories">
+              {serving.calories} calories
+            </div>
+          </div>
+          <div className="servingSizeBoxBottom">
+            <div className="servingProtein">
+              {serving.protein} p
+            </div>
+            <div className="servingCarbohydrate">
+              {serving.carbohydrate} c
+            </div>
+            <div className="servingFat">
+              {serving.fat} f
+            </div>
+          </div>
         </div>
-        <div className="servingProtein">
-          {serving.protein} protein
-        </div>
-        <div className="servingCarbohydrate">
-          {serving.carbohydrate} carbs
-        </div>
-        <div className="servingFat">
-          {serving.fat} fat
+        <div className="nutritionalContent">
+          <div className="nutritionFacts">
+            Nutrition Facts
+          </div>
+          <br />
+          Serving Size: {Math.round(serving.metric_serving_amount)} {serving.metric_serving_unit}
+          <div className="amountPerServing">
+            Amount Per Serving
+            <div className="ampCalories">
+              Calories {serving.calories}
+            </div>
+            <div className="ampFat">
+              Calories from Fat {Math.round(serving.fat * 9)}
+            </div>
+          </div>
+
         </div>
       </div>
     );
