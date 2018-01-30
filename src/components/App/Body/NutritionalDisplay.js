e18cd72c4f03c44bd0bf3159d49dd71bb29c3094
Christopher Lee <xchrislee@gmail.com>
2018-01-29 19:35:38 -0800

    Clean up extraneous code

--- src/components/App/Body/NutritionalDisplay.js
+++ src/components/App/Body/NutritionalDisplay.js
@@ -5,7 +5,7 @@ import ServingSizeInfoDisplay from './ServingSizeInfoDisplay';
 class NutritionalDisplay extends Component {
   constructor(props) {
     super(props);
-    this.state = {servingData: null};
+    this.state = { servingData: null };
   }
   componentWillReceiveProps(props) {
     console.log(props, "CWP IN NT")
@@ -16,8 +16,6 @@ class NutritionalDisplay extends Component {
     const { servingData } = this.state;
 
     let servingSizeDropdown = null;
-  
-
     if (Object.keys(nutritionalInfo).length > 0) {
       const servingOptions = nutritionalInfo.servings.serving.map((serving, i) => (
         {
@@ -27,9 +25,8 @@ class NutritionalDisplay extends Component {
       ));
 
       const handleServingDropdownChange = (e, data) => {
-        console.log(data.value, "hsddc");
-        this.setState({servingData:nutritionalInfo.servings.serving[data.value]});
-      }
+        this.setState({ servingData: nutritionalInfo.servings.serving[data.value] });
+      };
 
       servingSizeDropdown = (
         <Dropdown
@@ -48,69 +45,7 @@ class NutritionalDisplay extends Component {
         <ServingSizeInfoDisplay {...servingData} />
       </div>
     );
-  };
-
-    // let displayInfo = null;
-    // let servingsizeDropdown;
-    // let servingDropdown = null;
-
-    // if (Object.keys(nutritionalInfo).length > 0) {
-    //   console.log(nutritionalInfo, "nutritionalInfo")
-    //   const { 
-    //     id,
-    //     name,
-    //     servings,
-    //     type,
-    //     url,
-    //   } = nutritionalInfo;
-
-    //   const selectedServing = serving => (
-    //     <div>
-    //       {serving.name}
-    //       {serving.serving_description}
-    //       <div className='servingCalories'>
-    //         {serving.calories} calories
-    //       </div>
-    //       <div className='servingProtein'>
-    //         {serving.protein} protein
-    //       </div>
-    //       <div className='servingCarbohydrate'>
-    //         {serving.carbohydrate} carbs
-    //       </div>
-    //       <div className='servingFat'>
-    //         {serving.fat} fat
-    //       </div>
-    //     </div>
-    //   );
-
-    //   const handleServingDropdownChange = (e, data) => {
-    //     console.log(data.value, "serving drop down change")
-    //   } 
-// {selectedServing(servings.serving[0])}
-
-    //   displayInfo = (
-    //     <div>
-    //       {servings.serving.map((serving, key) => {
-    //         return (
-    //           <div key={key}>
-    //             { serving.serving_description }
-    //           </div>
-    //         )
-    //       })}
-    //     </div>
-    //   );
-
-    // if (this.state.dropdownSelected) {
-    //     servingDropdown = (
-    //       <Dropdown
-    //         placeholder="select serving size" 
-    //         selection
-    //         closeOnChange
-    //         onChange={handleServingDropdownChange} 
-    //         options={foodSearchResults} />
-    //     )
-    // }
-  // }
-    }
+  }
+}
 
 export default NutritionalDisplay;
